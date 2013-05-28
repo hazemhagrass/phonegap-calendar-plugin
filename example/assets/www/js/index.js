e71e13ce2e4f8f4ca38cf81539375021d041b196
Hazem Hagrass <hazem.hagrass@badrit.com>
2013-05-28 16:41:01 +0200

    Features: add all events info to table

--- example/assets/www/js/index.js
+++ example/assets/www/js/index.js
@@ -2,8 +2,14 @@ $(function(){
 	$('.find-events').live('click', function(){
 		alert('Finding events ....');
 		cordova.require('cordova/plugin/calendar').findEvents(function(events) {
+			$('.events_table tbody').empty();
 			$(events).each(function(index, value){
-				$('.events_table tbody').append("<tr><td>" + index + "</td></tr>");	
+				$('.events_table tbody').append("<tr> \
+					<td>" + value.id + "</td> \
+					<td>" + value.summary + "</td> \
+					<td>" + value.start + "</td> \
+					<td>" + value.end + "</td> \
+					</tr>");	
 			});
 		}, function() {
 			alert('Error');
