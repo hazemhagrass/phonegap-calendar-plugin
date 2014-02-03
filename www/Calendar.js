6a94467007d492dbe6f8a625102c1ee3e9c843b5
Mohamed Salah <mohamed.salah@badrit.com>
2014-02-03 14:53:28 +0200

    update xml and add ios plugin

--- www/Calendar.js
+++ www/Calendar.js
@@ -1,18 +1,40 @@
 var Calendar = {
   createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
-    cordova.exec(
-      successCallback,
-      errorCallback,
-      'CalendarPlugin',
-      'addToCalendar', [{
-        "title": title,
-        "description": notes,
-        "eventLocation": location,
+    if (device.platform == "Android")
+      cordova.exec(
+        successCallback,
+        errorCallback,
+        'CalendarPlugin',
+        'addToCalendar', [{
+          "title": title,
+          "description": notes,
+          "eventLocation": location,
+          "startTimeMillis": startDate.getTime(),
+          "endTimeMillis": endDate.getTime()
+        }]
+        );
+    else
+      cordova.exec(successCallback,errorCallback,"CalendarPlugin","createEvent", [title,location,notes,startDate,endDate]);
+  },
+  findEvents: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    if (device.platform == "Android")
+      cordova.exec(successCallback, errorCallback, 'CalendarPlugin', 'findFromCalendar', [{
         "startTimeMillis": startDate.getTime(),
         "endTimeMillis": endDate.getTime()
       }]
-    );
-  }
+      );
+    else
+      cordova.exec(successCallback,errorCallback,"CalendarPlugin","findEvent", [title,location,notes,startDate,endDate]);
+  },
+  deleteEvent: function(title, successCallback, errorCallback) {
+    if (device.platform == "Android")
+      cordova.exec(successCallback, errorCallback, 'CalendarPlugin', 'deleteFromCalendar', [{
+        "title": title
+      }]
+      );
+    else
+     cordova.exec(successCallback,errorCallback,"CalendarPlugin","deleteEvent", [title,location,notes,startDate,endDate, true]);
+ }
 };
 
 module.exports = Calendar;
